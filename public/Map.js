d05159cddb8ed887901f28bf99055ef562bc3595
Cole A. Kendall <kendall.cole@revature.com>
2022-06-17 20:54:46 -0400

    add boiler plate code for the Map.js file

---
+++ public/Map.js
@@ -0,0 +1,8 @@
+var Map = function() {
+    this.scenes = [];
+    this.current = null;
+    this.moveLeft = function () {};
+    this.moveRight = function () {};
+    this.moveUp = function () {};
+    this.moveDown = function () {};
+};
