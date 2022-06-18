d70ecb83155e88a32fb75df7350586c3826e6171
Cole A. Kendall <kendall.cole@revature.com>
2022-06-17 20:45:42 -0400

    refactor code to construct a game object that handles what this file initially did

--- public/index.js
+++ public/index.js
@@ -1,25 +1,2 @@
-
 const PATH_TO_REASOURCE_JSON = "Resources//specs.json";
-
-fetch(PATH_TO_REASOURCE_JSON)
-                .then(responce => responce.json())
-                .then(data => {init(data)});
-
-
-function init(ResourcesJSON) 
-{
-    let rendererWidth = ResourcesJSON.renderer.width;
-    let rendererHeight= ResourcesJSON.renderer.height;
-
-    let app = new PIXI.Application({ width: rendererWidth, height: rendererHeight });
-    document.body.appendChild(app.view);
-}
-
-function loadTextures(textureArr, folderPath, fileExtention, numOfFrames)
-{
-    for (let i=0; i < numOfFrames; i++)
-    {
-        let texture = PIXI.Texture.from(folderPath + i + fileExtention);
-        textureArr.push(texture);
-    };
-}
+var myGame = new Game(PATH_TO_REASOURCE_JSON);
