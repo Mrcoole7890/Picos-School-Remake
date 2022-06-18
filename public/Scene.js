95e9e76039d4d74459436e8cbe3bf7a9d108d866
Cole A. Kendall <kendall.cole@revature.com>
2022-06-18 00:24:26 -0400

    implemented scene initialization and asset loading

--- public/Scene.js
+++ public/Scene.js
@@ -1,10 +1,32 @@
-var Scene = function() {
+var Scene = function(JSONResources, JSONSceneName, app) {
+    this.app = app;
     this.entities = [];
     this.background;
     this.leftScene;
     this.rightScene;
     this.upScene;
     this.downScene;
+    this.JSONSceneName = JSONSceneName;
+    this.JSONScene = JSONResources.Scenes[JSONSceneName];
 
-    this.loadAssets = function() {};
+    this.init = function() {
+        this.JSONScene.entities.forEach(e => {
+            console.log(JSONResources.entities[e[0]]);
+            let entity = new UnanimatedEntity(JSONResources.entities[e[0]], e[1], e[2]);
+            this.entities.push(entity);
+        });
+        
+    };
+
+    this.loadAssets = function() {
+        this.entities.forEach(e => {
+            e.load(this.app);
+        });
+    };
+
+    this.unloadAssets = function() {
+        this.entities.forEach(e => {
+            e.unload();
+        });
+    };
 };
