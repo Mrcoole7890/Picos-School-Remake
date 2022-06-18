af0983db992d4ee333741d76a8bf7c5002dcd8e8
Kendallcole7@gmail.com <kendall.cole@revature.com>
2022-06-18 03:17:00 -0400

    add means to add custom functionality upon being clicked on

--- public/UnanimatedEntity.js
+++ public/UnanimatedEntity.js
@@ -1,21 +1,25 @@
 var UnanimatedEntity = function(JSONObject, x, y) {
     this.sprite;
-    this.onClick;
+    this.onClick = function() {};
     this.onHover;
+    this.JSONObject = JSONObject;
+    this.class = JSONObject.class;
     this.texturePath = JSONObject.texturePath;
     this.width = JSONObject.width;
     this.height = JSONObject.height;
+    this.interactible = JSONObject.interactible
     this.x = x;
     this.y = y;
     let spriteTexture = PIXI.Texture.from(this.texturePath);
-
     this.load = function(PIXIApplication) {
         this.sprite = new PIXI.Sprite(spriteTexture);
         this.sprite.width = this.width;
         this.sprite.height = this.height;
         this.sprite.x = this.x;
         this.sprite.y = this.y;
+        this.sprite.interactive = this.interactible;
         PIXIApplication.stage.addChild(this.sprite);
+        this.sprite.on('pointerdown', this.onClick );
     };
 
     this.unload = function() {
