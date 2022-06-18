3db3b20a0fe9d2c0f362528767dd297d373af4f5
Cole A. Kendall <kendall.cole@revature.com>
2022-06-18 01:12:11 -0400

    implement initialisation and asset loading withing the map.js file

--- public/Map.js
+++ public/Map.js
@@ -1,10 +1,23 @@
-var Map = function() {
+var Map = function(JSONResources, app) {
+    this.app = app;
     this.scenes = [];
-    this.current = null;
+    this.current;
     this.moveLeft = function() {};
     this.moveRight = function() {};
     this.moveUp = function() {};
     this.moveDown = function() {};
+    this.JSONResources = JSONResources;
 
-    this.loadAssets = function() {};
+    this.init = function() {
+        this.JSONResources.map.sceneGraph.forEach(e =>{
+            let tempScene = new Scene(this.JSONResources, e[0], this.app);
+            tempScene.init();
+            this.scenes.push(tempScene);
+            if (e[0] == this.JSONResources.map.start) this.current = tempScene;
+        });
+    };
+
+    this.loadAssets = function() {
+        this.current.loadAssets();
+    };
 };
